4e77c76cac74f64e3a8831e8ceac6b59be9ec711
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2025-04-02 18:32:12 +0200

    Updating deploy.bat file

--- deploy.bat
+++ deploy.bat
@@ -2,24 +2,48 @@
 
 @rem Dowloading dependencies
 @if not exist utils (
+	@echo Cloning git repo for project utils
     @call git clone --branch 2.0-SNAPSHOT --single-branch https://github.com/Pierre-Emmanuel41/utils
-	
-	@call cd utils
 ) else ( 
 	@call cd utils
-	
+
 	@rem Pulling latest changes
 	@call git pull
 )
 
-@echo Building project utils
+@if not exist protocol (
+	@echo Cloning git repo for project protocol
+    @call git clone --branch 1.0-SNAPSHOT --single-branch https://github.com/Pierre-Emmanuel41/protocol
+) else ( 
+	@call cd protocol
+
+	@echo Pulling latest changes for project protocol
+	@call git pull
+)
+
+@if not exist communication (
+	@echo Cloning git repo for project communication
+    @call git clone --branch 1.0-SNAPSHOT --single-branch https://github.com/Pierre-Emmanuel41/communication
+) else ( 
+	@call cd communication
+
+	@echo Pulling latest changes for project protocol
+	@call git pull
+)
 
 @rem Building dependencies
+@echo Building project utils
+@call cd ../utils
 @call mvn clean package install
 
-@call cd ../communication
+@echo Building project protocol
+@call cd ../protocol
+@call mvn clean package install
 
 @echo Building project communication
+@call cd ../communication
+@call mvn clean package install
 
-@rem Building project
+@echo Building project messenger
+@call cd ../messenger
 @call mvn clean package install
