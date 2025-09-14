eeeadb03df4a58a77ffc41318d89f6d43ec5b048
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2025-09-14 18:59:09 +0200

    Deleting feature related to client's privilege

--- src/main/java/fr/pederobien/messenger/interfaces/server/IProtocolClient.java
+++ src/main/java/fr/pederobien/messenger/interfaces/server/IProtocolClient.java
@@ -21,27 +21,13 @@ public interface IProtocolClient {
      * Register a request handler to execute when an unexpected request is received from the
      * server.
      *
-     * @param privilege  The minimum privilege level the client shall have to process the request.
      * @param identifier The request identifier.
      * @param handler    The handler to execute.
      */
-    void addRequestHandler(int privilege, IIdentifier identifier, IRequestHandler handler);
+    void addRequestHandler(IIdentifier identifier, IRequestHandler handler);
 
     /**
      * @return The connection to send /receive requests from the server.
      */
     IProtocolConnection getConnection();
-
-    /**
-     * @return The privilege level of this client. The higher it is, the less restriction there are to process requests.
-     */
-    int getPrivilege();
-
-    /**
-     * Set the privilege level of this client. By modifying this level, the client may be restricted to process some
-     * requests.
-     *
-     * @param privilege The privilege level of this client.
-     */
-    void setPrivilege(int privilege);
 }
