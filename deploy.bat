543daa03307c8a8e66bdb5d0780313c72b688d4e
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2025-10-17 19:22:26 +0200

    Fix: deploy.bat not deploying

--- deploy.bat
+++ deploy.bat
@@ -3,47 +3,56 @@
 @rem Downloading dependencies
 @if not exist utils (
 	@echo Cloning git repo for project utils
-    @call git clone https://github.com/Pierre-Emmanuel41/utils
+    @call git clone --branch 2.0-SNAPSHOT --single-branch https://github.com/Pierre-Emmanuel41/utils
 ) else ( 
 	@call cd utils
 
 	@rem Pulling latest changes
 	@call git pull
+
+	@call cd ..
 )
 
 @if not exist communication (
 	@echo Cloning git repo for project communication
-    @call git clone https://github.com/Pierre-Emmanuel41/communication
+    @call git clone --branch 2.0-SNAPSHOT --single-branch https://github.com/Pierre-Emmanuel41/communication
 ) else (
 	@call cd communication
 
 	@echo Pulling latest changes for project protocol
 	@call git pull
+
+	@call cd ..
 )
 
 @if not exist protocol (
 	@echo Cloning git repo for project protocol
-    @call git clone https://github.com/Pierre-Emmanuel41/protocol
+    @call git clone --branch 1.0-SNAPSHOT --single-branch https://github.com/Pierre-Emmanuel41/protocol
 ) else ( 
 	@call cd protocol
 
 	@echo Pulling latest changes for project protocol
 	@call git pull
+
+	@call cd ..
 )
 
 @rem Building dependencies
 @echo Building project utils
-@call cd ../utils
+@call cd utils
 @call mvn clean package install
+@call cd ..
 
 @echo Building project communication
-@call cd ../communication
+@call cd communication
 @call mvn clean package install
+@call cd ..
 
 @echo Building project protocol
-@call cd ../protocol
+@call cd protocol
 @call mvn clean package install
+@call cd ..
 
 @echo Building project messenger
-@call cd ../messenger
+@call cd messenger
 @call mvn clean package install
